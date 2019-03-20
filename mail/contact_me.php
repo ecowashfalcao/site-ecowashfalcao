2a3ce7a4e24d9ca0fcc6ca2d33519c3273b64853
EcoWash Falcão <ecowashfalcao@gmail.com>
2019-03-20 11:49:25 -0300

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -14,7 +14,7 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 $to = "ecowashfalcao@gmail.com"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
 $subject = "Orçamento via Site:  $name";
 $body = "Você está recebendo um novo Orçamento realizado pelo Site.\n\n"."Detalhes:\n\nNome: $name\n\nEmail: $email\n\nTelefone: $phone\n\nMessage:\n$message";
-$header = "De: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$header = "De: ecowashfalcao@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $header .= "Replicado-para: $email";	
 
 if(!mail($to, $subject, $body, $header))
